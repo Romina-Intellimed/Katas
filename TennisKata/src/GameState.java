813b00167147487664d9c429cbf9fd5f8995ab8e
Romina-Intellimed <romina88.tb@gmail.com>
2016-01-21 16:34:27 +0100

    create enumm for game states

---
+++ TennisKata/src/GameState.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Romina DRUTA on 21.01.2016.
+ */
+public enum GameState {
+
+    START(0), PLAY(1), IS_GAME_DEUCE(2), IS_GAME_ADVNATAGE(3), IS_GAME_WIN(4);
+
+    private int intValue;
+
+    GameState(int value) {
+        this.intValue = value;
+    }
+
+    public int getIntValue() {
+        return intValue;
+    }
+}

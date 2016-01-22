131c68bffc16ee44d890a4b14296c5b85b7b76ee
Romina-Intellimed <romina88.tb@gmail.com>
2016-01-22 11:55:55 +0100

    add first unit tests for converting a roman to numeral

--- KataRomanNumerals/test/training/names/ConversionRomanToNumeralTests.java
+++ KataRomanNumerals/test/training/names/ConversionRomanToNumeralTests.java
@@ -1,7 +1,49 @@
 package training.names;
 
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by Romina DRUTA on 22.01.2016.
  */
 public class ConversionRomanToNumeralTests {
+
+    private ConversionRomanToNumeral converter;
+
+    @Before
+    public void setup() {
+        converter = new ConversionRomanToNumeral();
+    }
+
+    @Test
+    public void dummyTest() {
+        assert true == true;
+    }
+
+    @Test
+    public void IshouldConvertTo1() {
+        String input = "I";
+        Integer expectedOutput = 1;
+
+        Integer actualOutput = converter.romanToNumeral(input);
+
+        Assert.assertEquals(input + " is converted to " + actualOutput, expectedOutput, actualOutput);
+    }
+
+
+
+    @Test
+    public void XshouldCOnvertTo10(){
+        String input = "X";
+        Integer expectedOutput=10;
+
+        Integer actualOutput=converter.romanToNumeral(input);
+
+        Assert.assertEquals(input + " is converted to "+actualOutput,expectedOutput,actualOutput);
+
+    }
+
+
+
 }

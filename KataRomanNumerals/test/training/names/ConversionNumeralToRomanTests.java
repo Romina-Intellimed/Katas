d97a7e5fcc3deb305f19f7c7db5e6da9a7db0142
Romina-Intellimed <romina88.tb@gmail.com>
2016-01-25 15:54:31 +0100

    Adding JUnit Tests Class for Conversion Numeral To Roman and setup function within this class

---
+++ KataRomanNumerals/test/training/names/ConversionNumeralToRomanTests.java
@@ -0,0 +1,23 @@
+package training.names;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Romina DRUTA on 25.01.2016.
+ */
+public class ConversionNumeralToRomanTests {
+
+
+    private ConversionNumeralToRoman converter;
+
+    @Before
+    public void setup() {
+        converter = new ConversionNumeralToRoman();
+    }
+
+    @Test
+    public void dummyTest() {
+        assert true;
+    }
+}

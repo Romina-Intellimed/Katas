701b36160b8cffd96a6e7658e167feb627300f63
romina <romina88.tb@gmail.com>
2016-01-26 14:08:12 +0200

    add JUnit test for random player returned

--- TennisKata/test/GameStateTest.java
+++ TennisKata/test/GameStateTest.java
@@ -2,4 +2,5 @@
  * Created by Romina DRUTA on 25.01.2016.
  */
 public class GameStateTest {
+
 }
--- TennisKata/test/TennisKataToolsTest.java
+++ TennisKata/test/TennisKataToolsTest.java
@@ -1,5 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * Created by Romina DRUTA on 25.01.2016.
  */
 public class TennisKataToolsTest {
+
+    private TennisKataTools tools;
+
+    @Before
+    public void setup() {
+        tools = new TennisKataTools();
+    }
+
+    @Test
+    public void dummyTest() {
+        assert true;
+    }
+
+
+    @Test
+    public void shouldReturnRandomPlayer(){
+        List<Player> inputPlayersList=new ArrayList<>();
+        inputPlayersList.add(new Player("player-test-1"));
+        inputPlayersList.add(new Player("player-test-2"));
+
+        Player expectedPlayer1Output=inputPlayersList.get(0);
+        Player expectedPlayer2Output=inputPlayersList.get(1);
+
+        Player actualPlayerOuput=tools.randomPlayer(inputPlayersList);
+
+        assertEquals(expectedPlayer1Output + " or " + expectedPlayer2Output+ " should be " + actualPlayerOuput, expectedPlayer1Output, actualPlayerOuput); //???
+
+
+    }
+
+    
+
+
+
 }

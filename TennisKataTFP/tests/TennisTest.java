52560327f855859a15fb9cd0fe79f70faf8e523f
Romina-Intellimed <romina88.tb@gmail.com>
2016-01-28 12:58:32 +0200

    add new project TennisKataTFP for first test programming and a first test class

---
+++ TennisKataTFP/tests/TennisTest.java
@@ -0,0 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by romina on 28.01.2016.
+ */
+public class TennisTest {
+
+    @Before
+    private void setup(){
+
+
+    }
+
+
+    @Test
+    void theGameHasTwoPlayers(){
+
+        Player inputPlayer1=new Player();
+        Player inputPlayer2=new Player();
+
+
+        Boolean expectedOutput=true;
+
+
+
+    }
+
+
+    @Test
+    void onePlayerShouldServe(){
+
+
+
+
+    }
+
+
+
+}

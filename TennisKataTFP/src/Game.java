d271d341a3a9066e127ebf64125ef56af55574ef
Romina-Intellimed <romina88.tb@gmail.com>
2016-01-28 13:29:26 +0200

    modify Game class by adding players

--- TennisKataTFP/src/Game.java
+++ TennisKataTFP/src/Game.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+
 /**
  * Created by romina on 28.01.2016.
  */
 public class Game {
+
+    ArrayList playersList;
+
+
+    Game(){
+        playersList=new ArrayList<>();
+        String player1="player1";
+        String player2="player2";
+
+        playersList.add(player1);
+        playersList.add(player2);
+
+
+    }
+
+
 }
--- TennisKataTFP/tests/TennisTest.java
+++ TennisKataTFP/tests/TennisTest.java
@@ -1,34 +1,40 @@
+import com.sun.media.jfxmedia.events.PlayerStateEvent;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by romina on 28.01.2016.
  */
 public class TennisTest {
 
+    Game testGame;
+
     @Before
-    private void setup(){
-
-
-    }
-
-
-    @Test
-    void theGameHasTwoPlayers(){
-
-        Player inputPlayer1=new Player();
-        Player inputPlayer2=new Player();
-
-
-        Boolean expectedOutput=true;
-
-
+    public void setup(){
+        testGame=new Game();
 
     }
 
 
     @Test
-    void onePlayerShouldServe(){
+    public void theGamesHasTwoPlayers(){
+
+        assertTrue(testGame.playersList.size()==2);
+
+    }
+
+
+
+
+
+    @Test
+    public void onePlayerShouldServe(){
 
 
 

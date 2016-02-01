a0be010d8eb22da0d1c1a0f803a4f860a4588d93
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-01 16:02:28 +0200

    add empty test cases and create RomanCalculator class

---
+++ RomanCalculator/src/RomanCalculator.groovy
@@ -0,0 +1,5 @@
+/**
+ * Created by romina on 01.02.2016.
+ */
+class RomanCalculator {
+}
---
+++ RomanCalculator/test/RomanCalculatorTest.groovy
@@ -0,0 +1,143 @@
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Created by romina on 01.02.2016.
+ */
+class RomanCalculatorTest {
+
+
+    def calculator
+
+    @Before
+    void setup() {
+        calculator = new RomanCalculator()
+    }
+
+
+    @Test
+    void IplusIisII() {
+
+
+    }
+
+    @Test
+    void VplusVisX() {
+
+
+    }
+
+
+    @Test
+    void XplusXisXX() {
+
+
+    }
+
+    @Test
+    void LplusLisC() {
+
+
+    }
+
+
+    @Test
+    void CplusCisCC() {
+
+
+    }
+
+    @Test
+    void DplusDisM() {
+
+
+    }
+
+
+    @Test
+    void MplusMisMM() {
+
+
+    }
+
+
+    @Test
+    void IIplusIIisIV() {
+
+
+    }
+
+
+    @Test
+    void XXplusXXisXL() {
+
+
+    }
+
+
+    @Test
+    void CCplusCCisCD() {
+
+
+    }
+
+
+    @Test
+    void IIIplusIIIisVI() {
+
+
+    }
+
+    @Test
+    void XXXplusXXXisLX() {
+
+
+    }
+
+
+    @Test
+    void CCCplusCCCisDC() {
+
+
+    }
+
+    @Test
+    void "I plus III is IV"() {
+
+    }
+
+    @Test
+    void "I plus VIII is IX"() {
+
+    }
+
+    @Test
+    void "I plus XIII is XIV"() {
+
+    }
+
+    @Test
+    void "I plus XVIII is XIX"() {
+
+    }
+
+    @Test
+    void "X plus XXX is XL"() {
+
+    }
+
+    @Test
+    void "X plus LXXX is XC"() {
+
+    }
+
+    @Test
+    void "V plus IV is IX"() {
+
+    }
+
+    @Test
+    void "D plus CD is CM"() {
+
+    }
+}

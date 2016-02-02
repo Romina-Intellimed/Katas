646e6acba8ae12ecf8e6b66e23d63711ed247554
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-02 14:58:20 +0200

    refactor code for substractive groups in roman number

--- RomanCalculator/src/RomanCalculator.groovy
+++ RomanCalculator/src/RomanCalculator.groovy
@@ -4,32 +4,34 @@
 class RomanCalculator {
 
     def romanDigitValue = ['I': 1, 'V': 2, 'X': 3, 'L': 4, 'C': 5, 'D': 6, 'M': 7]
-    def romanSameConsecutiveDigitEquivalence = ['IIIII':'V', 'VV':'X', 'XXXXX':'L', 'LL':'C', 'CCCCC':'D', 'DD':'M']
-    def romanSubstractivesEquivalence =['IIII':'IV', 'VIIII':'IX', 'XXXX':'XL', 'LXXXX':'XC', 'CCCC':'CD', 'DCCCC':'CM']
+    def romanSameConsecutiveDigitEquivalence = ['IIIII': 'V', 'VV': 'X', 'XXXXX': 'L', 'LL': 'C', 'CCCCC': 'D', 'DD': 'M']
+    def romanSubstractivesEquivalence = ['IIII': 'IV', 'VIIII': 'IX', 'XXXX': 'XL', 'LXXXX': 'XC', 'CCCC': 'CD', 'DCCCC': 'CM']
 
 
-    def sumRomanNumbers(firstRoman, secondRoman){
-
-        romanSubstractivesEquivalence.each { entry ->
-            firstRoman = firstRoman.replaceAll(entry.value, entry.key)
-        }
-
-        romanSubstractivesEquivalence.each { entry ->
-            secondRoman = secondRoman.replaceAll(entry.value, entry.key)
-        }
+    def sumRomanNumbers(firstRoman, secondRoman) {
 
 
-        def digitsFromRomansToSumUp = firstRoman + secondRoman
-        digitsFromRomansToSumUp = digitsFromRomansToSumUp.toList().sort{romanDigitValue.get(it)}.reverse().join()
+        def digitsFromRomansToSumUp = replaceSubstractiveGroups(firstRoman) + replaceSubstractiveGroups(secondRoman)
+        digitsFromRomansToSumUp = digitsFromRomansToSumUp.toList().sort { romanDigitValue.get(it) }.reverse().join()
 
 
         romanSameConsecutiveDigitEquivalence.keySet().each { key ->
             digitsFromRomansToSumUp = digitsFromRomansToSumUp.replaceAll(key, romanSameConsecutiveDigitEquivalence.get(key))
         }
         romanSubstractivesEquivalence.keySet().iterator().reverse().each { key ->
-            digitsFromRomansToSumUp = digitsFromRomansToSumUp.replaceAll(key, romanSubstractivesEquivalence.get(key))}
+            digitsFromRomansToSumUp = digitsFromRomansToSumUp.replaceAll(key, romanSubstractivesEquivalence.get(key))
+        }
 
-       return digitsFromRomansToSumUp
+        return digitsFromRomansToSumUp
     }
+
+    def replaceSubstractiveGroups(romanNumber) {
+        romanSubstractivesEquivalence.each { entry ->
+            romanNumber = romanNumber.replaceAll(entry.value, entry.key)
+        }
+        return romanNumber
+
+    }
+
 
 }

2a49a4f3df95f62346d10bf96c95d45cfbcf458c
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-08 16:47:20 +0200

    first commit - add the two first tests

---
+++ FizzBuzz/src/FizzBuzz.groovy
@@ -0,0 +1,11 @@
+/**
+ * Created by romina on 08.02.2016.
+ */
+class FizzBuzz {
+
+    def printResult(int inputNumber){
+        inputNumber.toString()
+
+    }
+
+}
---
+++ FizzBuzz/test/FizzBuzzGameSpec.groovy
@@ -0,0 +1,99 @@
+import spock.lang.Specification
+
+/**
+ * Created by romina on 08.02.2016.
+ */
+class FizzBuzzGameSpec extends Specification {
+    FizzBuzz fizzBuzz
+    void setup(){
+        fizzBuzz =new FizzBuzz();
+    }
+
+    void "for 1 return 1"(){
+        given:
+        def inNumber = 1
+
+        when:
+        def output=fizzBuzz.printResult(inNumber)
+
+        then:
+        "1" == output
+
+    }
+
+    void "for 2 return 2 "(){
+
+        expect:
+        "2"==fizzBuzz.printResult(2)
+    }
+
+
+    void "for 3 return Fizz"(){
+
+    }
+
+    void "for 4 return 4"(){
+
+    }
+
+    void "for 5 return Buzz"(){
+
+    }
+
+    void "for 6 return Fizz"(){
+
+    }
+
+    void "for 7 return 7"(){
+
+    }
+
+    void "for 9 return Fizz"(){
+
+    }
+
+
+    void "for 10 return Buzz"(){
+
+    }
+
+
+    void "for 12 return Fizz"(){
+
+    }
+
+
+    void "for 15 return FizzBuzz"(){
+
+    }
+
+    void "for 18 return Fizz"(){
+
+    }
+
+    void "for 20 return Buzz"(){
+
+    }
+
+    void "for 30 return FizzBuzz"(){
+
+    }
+
+
+    void "for empty string return error input should not be empty"(){
+
+    }
+
+    void "for negative number return error input is a positive number"(){
+
+    }
+
+    void "for string input return error input should be a number"(){
+
+    }
+    void "for number bigger than 100 return error input should be smaller than 100"(){
+
+    }
+
+
+}

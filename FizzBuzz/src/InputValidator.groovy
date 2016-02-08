c22b6044e7a07aff763b37f1d858587e057e8524
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-08 18:16:46 +0200

    modify code for validateInput

--- FizzBuzz/src/InputValidator.groovy
+++ FizzBuzz/src/InputValidator.groovy
@@ -6,11 +6,9 @@ class InputValidator {
     static void validateInput(inputNumber) {
         if (inputNumber == "")
             throw new IllegalInputException("The input should not be empty")
-        if (inputNumber < 0)
-            throw new IllegalInputException("The input should be a positive number")
+        if (inputNumber < 0 || inputNumber >100)
+            throw new IllegalInputException("The input should be a positive number smaller than 100")
         if (inputNumber instanceof String)
             throw new IllegalInputException("The input should be a positive number")
-        if (inputNumber > 100)
-            throw new IllegalInputException("The input should be a positive number smaller than 100")
     }
 }

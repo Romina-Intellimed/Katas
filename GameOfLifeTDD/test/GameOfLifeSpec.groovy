5eb7749821a304f1988255d0d7805679c1ffaa6d
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-09 13:37:26 +0200

    extract test method for generating a 2x2 dead grid

--- GameOfLifeTDD/test/GameOfLifeSpec.groovy
+++ GameOfLifeTDD/test/GameOfLifeSpec.groovy
@@ -6,70 +6,71 @@ import spock.lang.Specification
 class GameOfLifeSpec extends Specification {
 
     GameOfLife gameOfLife
+
     void setup() {
-        gameOfLife=new GameOfLife()
+        gameOfLife = new GameOfLife()
     }
 
-    void "a 2x2 grid with no live cells will have no live cell in next generation"(){
+    void "a 2x2 grid with no live cells will have no live cell in next generation"() {
         given:
-        def actualGrid=[[CellType.dead,CellType.dead],[CellType.dead,CellType.dead]]
+        def actualGrid = grid2x2WithDeadCells()
         when:
-        def expectedGrid= gameOfLife.generationGridEvolution(actualGrid)
+        def expectedGrid = gameOfLife.generationGridEvolution(actualGrid)
         then:
-        expectedGrid == [[CellType.dead,CellType.dead],[CellType.dead,CellType.dead]]
+        expectedGrid == grid2x2WithDeadCells()
     }
 
 
-    void "a 2x2 grid with one live cells will have no live cell in next generation"(){
+    void "a 2x2 grid with one live cells will have no live cell in next generation"() {
         given:
-        def actualGrid=[[CellType.alive,CellType.dead],[CellType.dead,CellType.dead]]
+        def actualGrid = [[CellType.alive, CellType.dead], [CellType.dead, CellType.dead]]
         when:
-        def expectedGrid= gameOfLife.generationGridEvolution(actualGrid)
+        def expectedGrid = gameOfLife.generationGridEvolution(actualGrid)
         then:
-        expectedGrid == [[CellType.dead,CellType.dead],[CellType.dead,CellType.dead]]
+        expectedGrid == grid2x2WithDeadCells()
+    }
+
+    private def grid2x2WithDeadCells() {
+        [[CellType.dead, CellType.dead], [CellType.dead, CellType.dead]]
     }
 
 
-    void "a 2x2 grid with two live cells will have no live cell in next generation"(){
-
-    }
-
-
-    void "a 2x2 grid with three live cells will have four live cells in next generation"(){
-
-    }
-
-
-    void "a 2x2 grid with four live cells will have no live cells in next generation"(){
+    void "a 2x2 grid with two live cells will have no live cell in next generation"() {
 
     }
 
 
-    void "a 3x3 grid with one live cells will have no live cell in next generation"(){
+    void "a 2x2 grid with three live cells will have four live cells in next generation"() {
 
     }
 
 
-
-    void "a 3x3 grid with two live cells will have no live cell in next generation"(){
-
-    }
-
-
-
-    void "a 3x3 grid with three live cells on the position: (CellType.dead,CellType.dead),(CellType.dead,CellType.alive),(CellType.dead,2) will have one live cell in next generation"(){
-
-    }
-
-    void "a 3x3 grid with three live cells on the position: (CellType.dead,CellType.dead),(CellType.dead,CellType.alive),(CellType.alive,CellType.dead) will have four live cells in next generation"(){
-
-    }
-
-    void "a 3x3 grid with three live cells on the position: (CellType.dead,CellType.dead),(CellType.dead,CellType.alive),(CellType.alive,CellType.alive) will have six live cells in next generation"(){
+    void "a 2x2 grid with four live cells will have no live cells in next generation"() {
 
     }
 
 
+    void "a 3x3 grid with one live cells will have no live cell in next generation"() {
+
+    }
+
+
+    void "a 3x3 grid with two live cells will have no live cell in next generation"() {
+
+    }
+
+
+    void "a 3x3 grid with three live cells on the position: (CellType.dead,CellType.dead),(CellType.dead,CellType.alive),(CellType.dead,2) will have one live cell in next generation"() {
+
+    }
+
+    void "a 3x3 grid with three live cells on the position: (CellType.dead,CellType.dead),(CellType.dead,CellType.alive),(CellType.alive,CellType.dead) will have four live cells in next generation"() {
+
+    }
+
+    void "a 3x3 grid with three live cells on the position: (CellType.dead,CellType.dead),(CellType.dead,CellType.alive),(CellType.alive,CellType.alive) will have six live cells in next generation"() {
+
+    }
 
 
 }

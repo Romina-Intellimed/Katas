38f14da72d3c9b48e9513bcc1934e7de294171a6
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-10 16:32:49 +0200

    refactor: move methods in other class

--- GameOfLifeTDD/src/GameOfLife.groovy
+++ GameOfLifeTDD/src/GameOfLife.groovy
@@ -2,41 +2,12 @@
  * Created by romina on 09.02.2016.
  */
 class GameOfLife {
-    def newGrid = []
 
     def generationGridEvolution(grid) {
-        nextGenerationGridWithDifferentTypeCells(grid)
-    }
-
-    def nextGenerationGridWithDifferentTypeCells(grid) {
-        (grid.size()).times { x ->
-            newGrid[x] = []
-            (grid.size()).times { y ->
-                def aliveNeighbours = NextGenerationGridBuilder.countAliveCellNeighbours(grid, x + 1, y + 1)
-                if (grid[x][y] == CellType.alive) {
-                    updateNextGenerationStateForAliveCell(aliveNeighbours, x, y)
-                } else {
-                    updateNextGenerationStateForDeadCell(aliveNeighbours, x, y)
-                }
-            }
-        }
-        newGrid
+        NextGenerationGridBuilder.nextGenerationGridWithDifferentTypeCells(grid)
     }
 
 
-    def updateNextGenerationStateForAliveCell(aliveNeighbours, x, y) {
-        if (aliveNeighbours < 2 || aliveNeighbours > 3)
-            newGrid[x][y] = CellType.dead
-        if (aliveNeighbours == 2 || aliveNeighbours == 3)
-            newGrid[x][y] = CellType.alive
-    }
-
-    def updateNextGenerationStateForDeadCell(aliveNeighbours, x, y) {
-        if (aliveNeighbours == 3) {
-            newGrid[x][y] = CellType.alive
-        } else
-            newGrid[x][y] = CellType.dead
-    }
 }
 
 
--- GameOfLifeTDD/src/NextGenerationGridBuilder.groovy
+++ GameOfLifeTDD/src/NextGenerationGridBuilder.groovy
@@ -4,6 +4,8 @@
 class NextGenerationGridBuilder {
 
 
+    def static newGrid = []
+
     def static surroundGridwithDeadCells(grid) {
         def gridToCheck = []
         (grid.size()+2).times { x ->
@@ -32,5 +34,34 @@ class NextGenerationGridBuilder {
 
         }
         aliveNeighbours
+    }
+
+    def static nextGenerationGridWithDifferentTypeCells(grid) {
+        (grid.size()).times { x ->
+            newGrid[x] = []
+            (grid.size()).times { y ->
+                def aliveNeighbours = NextGenerationGridBuilder.countAliveCellNeighbours(grid, x + 1, y + 1)
+                if (grid[x][y] == CellType.alive) {
+                    updateNextGenerationStateForAliveCell(aliveNeighbours, x, y)
+                } else {
+                    updateNextGenerationStateForDeadCell(aliveNeighbours, x, y)
+                }
+            }
+        }
+        newGrid
+    }
+
+    def static updateNextGenerationStateForAliveCell(aliveNeighbours, x, y) {
+        if (aliveNeighbours < 2 || aliveNeighbours > 3)
+            newGrid[x][y] = CellType.dead
+        if (aliveNeighbours == 2 || aliveNeighbours == 3)
+            newGrid[x][y] = CellType.alive
+    }
+
+    def static updateNextGenerationStateForDeadCell(aliveNeighbours, x, y) {
+        if (aliveNeighbours == 3) {
+            newGrid[x][y] = CellType.alive
+        } else
+            newGrid[x][y] = CellType.dead
     }
 }

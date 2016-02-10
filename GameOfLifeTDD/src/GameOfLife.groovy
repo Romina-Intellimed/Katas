8a69438df23af80ce2bc8ccc233225ec6a52d80a
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-10 18:06:23 +0200

    move method to GridBuilder

--- GameOfLifeTDD/src/GameOfLife.groovy
+++ GameOfLifeTDD/src/GameOfLife.groovy
@@ -5,19 +5,7 @@ class GameOfLife {
 
 
     def generationGridEvolution(grid) {
-        nextGenerationGridWithDifferentTypeCells(grid)
-    }
-
-    def static nextGenerationGridWithDifferentTypeCells(grid) {
-        def newGrid = []
-        (grid.size()).times { x ->
-            newGrid[x] = []
-            (grid.size()).times { y ->
-                def aliveNeighbours = Grid.countAliveCellNeighbours(grid, x + 1, y + 1)
-                newGrid[x][y] = Cell.updateCellState(aliveNeighbours, grid[x][y])
-            }
-        }
-        newGrid
+        GridBuilder.nextGenerationGridWithDifferentTypeCells(grid)
     }
 
 

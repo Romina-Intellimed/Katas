637ee9cc802a7a6834e1fd30d0de5357780ffcac
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-11 18:10:16 +0200

    add test for mock with grid and stub with grid - don't know if it is right

--- GameOfLifeTDD/test/GameOfLifeMocksAndStubsSpec.groovy
+++ GameOfLifeTDD/test/GameOfLifeMocksAndStubsSpec.groovy
@@ -3,7 +3,44 @@ import spock.lang.Specification
 /**
  * Created by romina on 11.02.2016.
  */
-class GameOfLifeMocksAndStubsSpec extends Specification{
+class GameOfLifeMocksAndStubsSpec extends Specification {
+
+    // Strict collaboration test using mock: universe.evolve() calls cell.evolve() only once
+    def "universe with a cell delegates evolution to cell"() {
+        given:
+        def grid = GenerationGridBuilder.aGenerationGridBuilder().withSize(3).build()
+
+        def gridBuilderMock = Mock(GridBuilder)
+        def gameOfLife = new GameOfLife()
+        gameOfLife.gridBuilder = gridBuilderMock
+
+        when:
+        gameOfLife.generationGridEvolution(grid)
+
+        then:
+        1 * gridBuilderMock.nextGenerationGrid(grid)
+    }
 
 
+    def "test stub for grid"() {
+        given:
+        def grid = []
+        def evolvedGrid = GenerationGridBuilder.aGenerationGridBuilder().withSize(3).withAliveCell(0,1).build()
+
+        def gridStub = Mock(GridBuilder) {
+            nextGenerationGrid(grid) >> evolvedGrid
+        }
+
+
+        def gameOfLife = new GameOfLife()
+
+
+
+        when:
+        gameOfLife.generationGridEvolution(grid)
+
+        then:
+        gridStub.nextGenerationGrid(grid) == evolvedGrid
+
+    }
 }

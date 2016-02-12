34398e5ff3fcf188a2f1fa3682f821eb56a871fd
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-12 18:56:23 +0200

    add method to GridBuilderSpec

--- GameOfLifeTDD/test/GridBuilderSpec.groovy
+++ GameOfLifeTDD/test/GridBuilderSpec.groovy
@@ -3,7 +3,7 @@ import spock.lang.Specification
 /**
  * Created by romina on 12.02.2016.
  */
-class GridBuilderSpec extends  Specification{
+class GridBuilderSpec extends Specification {
 
     def GridBuilder gridBuilder
 
@@ -11,6 +11,19 @@ class GridBuilderSpec extends  Specification{
         gridBuilder = new GridBuilder()
     }
 
+    void "test next generation grid state"() {
+        given:
+        def grid = GenerationGridBuilder.aGenerationGridBuilder().withSize(3).withAliveCell(0, 0).withAliveCell(1, 0).withAliveCell(2, 0).build()
+
+        def cellMock = Mock(CellManager)
+        def gridBuilder = new GridBuilder(cellManager: cellMock)
+        when:
+        gridBuilder.nextGenerationGrid(grid)
+        then:
+        9 * cellMock.updateCellState(*_)
+
+
+    }
 
 
 }

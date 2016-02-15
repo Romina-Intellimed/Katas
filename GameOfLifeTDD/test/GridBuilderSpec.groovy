c635832afb7db2e8a139750b9e014a75bb1e2c0f
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-15 16:58:56 +0200

    Data driven test for neighbours count

--- GameOfLifeTDD/test/GridBuilderSpec.groovy
+++ GameOfLifeTDD/test/GridBuilderSpec.groovy
@@ -1,4 +1,5 @@
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * Created by romina on 12.02.2016.
@@ -6,9 +7,13 @@ import spock.lang.Specification
 class GridBuilderSpec extends Specification {
 
     def GridBuilder gridBuilder
+    def cellManagerStub
+    def cellManagerMock
 
     def setup() {
         gridBuilder = new GridBuilder()
+        cellManagerStub = Mock(CellManager)
+        cellManagerMock = Mock(CellManager)
     }
 
     void "test next generation grid collaboration"() {
@@ -25,19 +30,65 @@ class GridBuilderSpec extends Specification {
 
     void "new grid contains cells returned by updateCellState"() {
         given:
-        def grid = GenerationGridBuilder.aGenerationGridBuilder().withSize(3).build()
-
-        def cellManagerStub = Mock(CellManager) {
-            updateCellState(*_) >> CellType.alive
-        }
-
+        cellManagerStub.updateCellState(*_) >> CellType.alive
         def gridBuilder = new GridBuilder(cellManager: cellManagerStub)
 
         when:
-        def newGrid = gridBuilder.nextGenerationGrid(grid)
+        def newGrid = gridBuilder.nextGenerationGrid(emptyGrid())
 
         then:
         newGrid == [[CellType.alive] * 3] * 3
     }
 
+    void "counts neighbours correctly when no neighbours"() {
+        given:
+        def gridBuilder = new GridBuilder(cellManager: cellManagerMock)
+
+        when:
+        gridBuilder.nextGenerationGrid(gridWithOneLiveCell())
+
+        then:
+        1 * cellManagerMock.updateCellState(0, CellType.alive)
+    }
+
+    void "counts neighbours correctly when one neighbour"() {
+        given:
+        def gridBuilder = new GridBuilder(cellManager: cellManagerMock)
+
+        when:
+        gridBuilder.nextGenerationGrid(gridWithTwoNeighbouringLiveCells())
+
+        then:
+        2 * cellManagerMock.updateCellState(1, CellType.alive)
+    }
+
+    @Unroll
+    void "counts neighbours correctly when #neighboursCount neighbours"(grid, liveCells, neighboursCount) {
+        given:
+        def gridBuilder = new GridBuilder(cellManager: cellManagerMock)
+
+        when:
+        gridBuilder.nextGenerationGrid(grid)
+
+        then:
+        liveCells * cellManagerMock.updateCellState(neighboursCount, CellType.alive)
+
+        where:
+        grid                               | liveCells || neighboursCount
+        emptyGrid()                        | 0         || 0
+        gridWithOneLiveCell()              | 1         || 0
+        gridWithTwoNeighbouringLiveCells() | 2         || 1
+    }
+
+    private ArrayList emptyGrid() {
+        GenerationGridBuilder.aGenerationGridBuilder().withSize(3).build()
+    }
+
+    private ArrayList gridWithOneLiveCell() {
+        GenerationGridBuilder.aGenerationGridBuilder().withSize(3).withAliveCell(0, 0).build()
+    }
+
+    private ArrayList gridWithTwoNeighbouringLiveCells() {
+        GenerationGridBuilder.aGenerationGridBuilder().withSize(3).withAliveCell(0, 0).withAliveCell(0, 1).build()
+    }
 }

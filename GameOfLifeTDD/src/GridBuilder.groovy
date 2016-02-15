2cb629f178377c043e75b967ccc77c464f876b53
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-15 17:29:21 +0200

    Simplified valid position checking

--- GameOfLifeTDD/src/GridBuilder.groovy
+++ GameOfLifeTDD/src/GridBuilder.groovy
@@ -11,7 +11,7 @@ class GridBuilder {
         (grid.size()).times { x ->
             newGrid[x] = []
             (grid.size()).times { y ->
-                def aliveNeighbours = countAliveCellNeighbours(grid, x + 1, y + 1)
+                def aliveNeighbours = countAliveCellNeighbours(grid, x, y)
                 newGrid[x][y] = cellManager.updateCellState(aliveNeighbours, grid[x][y])
             }
         }
@@ -20,28 +20,22 @@ class GridBuilder {
 
     private def countAliveCellNeighbours(grid, xPos, yPos) {
         def neighbours = [[xPos - 1, yPos - 1], [xPos - 1, yPos], [xPos - 1, yPos + 1], [xPos, yPos - 1], [xPos, yPos + 1], [xPos + 1, yPos - 1], [xPos + 1, yPos], [xPos + 1, yPos + 1]]
-        def surroundedGrid = surroundGridWithDeadCells(grid)
 
         return neighbours.inject(0) { aliveNeighbours, neighbourPosition ->
-            (surroundedGrid[neighbourPosition[0]][neighbourPosition[1]] == CellType.alive) ? aliveNeighbours + 1 : aliveNeighbours
+            hasNeighbour(grid, neighbourPosition) ? aliveNeighbours + 1 : aliveNeighbours
         }
     }
 
-    private def surroundGridWithDeadCells(grid) {
-        def gridToCheck = []
-        (grid.size() + 2).times { x ->
-            gridToCheck[x] = []
-            (grid.size() + 2).times { y ->
-                gridToCheck[x][y] = CellType.dead
-            }
-        }
-
-        (gridToCheck.size() - 2).times { x ->
-            (gridToCheck.size() - 2).times { y ->
-                gridToCheck[x + 1][y + 1] = grid[x][y]
-            }
-        }
-        gridToCheck
+    private boolean hasNeighbour(grid, neighbourPosition) {
+        def xPosition = neighbourPosition[0]
+        def yPosition = neighbourPosition[1]
+        
+        return isPositionValid(xPosition, grid) &&
+                isPositionValid(yPosition, grid) &&
+                grid[xPosition][yPosition] == CellType.alive
     }
 
+    private boolean isPositionValid(position, grid) {
+        return position in (0..grid.size() - 1)
+    }
 }

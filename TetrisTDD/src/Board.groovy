d3d727a9a2d2c7ad1283f5ee359e775b2ac34228
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-16 16:09:56 +0200

    delete printlns

--- TetrisTDD/src/Board.groovy
+++ TetrisTDD/src/Board.groovy
@@ -33,13 +33,13 @@ class Board {
         board[fallingBlock.xPos-1][fallingBlock.yPos] = 0
     }
 
-    def hasArrivedAtBottom() {
+    def blockHasArrivedAtBottom() {
         if (fallingBlock.xPos == (board.size()-1))
             return true
     }
 
 
-    def hasReachedAnotherBlock() {
+    def blockHasReachedAnotherBlock() {
         if ((fallingBlock.xPos < (board.size()-1) && board[1][1] == 1) || (board[2][1] == 1 && fallingBlock.xPos < board.size()-1))
             return true
     }

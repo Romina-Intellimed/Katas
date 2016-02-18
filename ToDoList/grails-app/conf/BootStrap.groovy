ba108a9efe421ea24897a4615e97789729d8a46e
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-18 17:24:16 +0200

    add two todo items in the database

--- ToDoList/grails-app/conf/BootStrap.groovy
+++ ToDoList/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,10 @@
+import todolist.ToDoItem
+
 class BootStrap {
 
     def init = { servletContext ->
+        new ToDoItem(name: "Learn Grails").save(failOnError: true)
+        new ToDoItem(name: "Learn Groovy").save(failOnError: true)
     }
     def destroy = {
     }

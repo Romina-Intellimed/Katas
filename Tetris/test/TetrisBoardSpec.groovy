b4f3c831690faecdabadb70526fc26ba4e23bd2d
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-24 13:45:52 +0200

    add TetrisBoardSpec

---
+++ Tetris/test/TetrisBoardSpec.groovy
@@ -0,0 +1,17 @@
+import spock.lang.Specification
+
+/**
+ * Created by romina on 24.02.2016.
+ */
+class TetrisBoardSpec extends Specification {
+
+    def tetrisBoard
+
+    void setup(){
+        tetrisBoard=new TetrisBoard()
+    }
+
+
+
+
+}

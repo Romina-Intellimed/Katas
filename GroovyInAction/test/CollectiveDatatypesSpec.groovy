865a20ec618e260fc0e133f6febb04d783abf6d3
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-24 11:14:10 +0200

    add other tests with lists

--- GroovyInAction/test/CollectiveDatatypesSpec.groovy
+++ GroovyInAction/test/CollectiveDatatypesSpec.groovy
@@ -5,18 +5,48 @@ import spock.lang.Specification
  */
 class CollectiveDatatypesSpec extends Specification {
 
-    def setup(){
-
+    List myList
+    def setup() {
+        myList = [1, 2, 3]
     }
 
 
-    void "test list with elements"(){
-        when:
-        List myList = [1,2,3]
-        then:
-        assert myList.size()==3
-        assert myList[0]==1
+    void "test list with elements"() {
+        expect:
+        assert myList.size() == 3
+        assert myList[0] == 1
         assert myList instanceof ArrayList
+    }
+
+    void "test empty list"() {
+        when:
+        List emptyList = []
+        then:
+        assert emptyList.size() == 0
+
+    }
+
+    void "test other type of lists"(){
+        when:
+        List longList=(0..1000).toList()
+        List explicitList=new ArrayList()
+        explicitList.addAll(myList)
+        explicitList[0]=10
+
+
+        then:
+        assert longList[555]==555
+        assert explicitList.size()==3
+        assert explicitList[0]==10
+
+
+        when:
+        explicitList=new LinkedList(myList)
+        explicitList[0]=10
+        then:
+        assert explicitList.size()==3
+        assert explicitList[0]==10
+
     }
 
 }

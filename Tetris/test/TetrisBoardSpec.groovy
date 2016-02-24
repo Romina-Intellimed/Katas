a2d7fbf4259a920656bcabfa5f568000996a2216
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-24 14:35:43 +0200

    add test for get block

--- Tetris/test/TetrisBoardSpec.groovy
+++ Tetris/test/TetrisBoardSpec.groovy
@@ -34,7 +34,16 @@ class TetrisBoardSpec extends Specification {
         tetrisBoard.removeBlock(0,0)
         then:
         assert tetrisBoard.board[0][0]==0
+    }
 
+    void "test get clok from a 1x1 board"(){
+        given:
+        tetrisBoard.initTheBoard(1,0)
+        when:
+        tetrisBoard.addBlock(0,0)
+        def block = tetrisBoard.getBlock(0,0)
+        then:
+        assert block==1
 
     }
 

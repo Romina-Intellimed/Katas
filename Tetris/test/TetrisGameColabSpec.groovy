ef7d7db4532415d285ce1829c573dd1be19ed429
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-25 14:03:38 +0200

    add test in isolation for TetrisGame

--- Tetris/test/TetrisGameColabSpec.groovy
+++ Tetris/test/TetrisGameColabSpec.groovy
@@ -3,27 +3,39 @@ import spock.lang.Specification
 /**
  * Created by romina on 25.02.2016.
  */
-class TetrisGameColabSpec extends Specification{
+class TetrisGameColabSpec extends Specification {
 
     def tetrisGame
 
-    void setup(){
-        tetrisGame=new TetrisGame()
+    void setup() {
+        tetrisGame = new TetrisGame()
     }
 
-    void "test add block is called once when board is mocked "(){
+    void "test addblock is called once when board is mocked "() {
 
         given:
-        def board=Mock(TetrisBoard)
-        tetrisGame.tetrisBoard=board
+        def boardMock = Mock(TetrisBoard)
+        tetrisGame.tetrisBoard = boardMock
 
         when:
         tetrisGame.dropBlock()
         then:
-        1*board.addBlock(*_)
+        1 * boardMock.addBlock(*_)
     }
 
+    void "test tetrisGame in isolation"() {
 
+        given:
+        def boardStub = Mock(TetrisBoard)
+        boardStub.initTheBoard(*_) >> BoardBuilder.aBoard().withSize(2, 2).buildBoard()
+        def tetrisGame = new TetrisGame(tetrisBoard: boardStub)
+        when:
+        def board= tetrisGame.tetrisBoard.initTheBoard(2,2)
+        then:
+        assert board == BoardBuilder.aBoard().withSize(2, 2).buildBoard()
+
+
+    }
 
 
 }

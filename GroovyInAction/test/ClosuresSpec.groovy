18b013eff32a1130a999defda941024746c78004
Romina-Intellimed <romina88.tb@gmail.com>
2016-02-27 11:57:00 +0200

    test areviated closures

--- GroovyInAction/test/ClosuresSpec.groovy
+++ GroovyInAction/test/ClosuresSpec.groovy
@@ -1,9 +1,25 @@
+import spock.lang.Ignore
 import spock.lang.Specification
 
 /**
  * Created by romina on 26.02.2016.
  */
-class ClosuresSpec extends  Specification{
+class ClosuresSpec extends Specification {
+
+
+    @Ignore
+    void "test abreviated clossures"() {
+        given:
+        def log = ''
+        (1..10).each { counter -> log += counter }
+        def logIt = ''
+        (1..10).each { it -> log += it }
+        expect:
+        assert log == "12345678910"
+        assert logIt == "12345678910"
+    }
+
+
 
 
 }

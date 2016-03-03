59810a1579d56d127f972e0868e4c9c3d1b8bf57
Romina-Intellimed <romina88.tb@gmail.com>
2016-03-03 14:50:29 +0200

    add tests for ToDoItemControllerSpec: test the return of the lists

--- ToDoList/test/unit/todolist/ToDoItemControllerSpec.groovy
+++ ToDoList/test/unit/todolist/ToDoItemControllerSpec.groovy
@@ -1,5 +1,6 @@
 package todolist
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,6 +8,7 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(ToDoItemController)
+@Mock([ToDoItem])
 class ToDoItemControllerSpec extends Specification {
 
     def setup() {
@@ -15,6 +17,23 @@ class ToDoItemControllerSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
+    void "index returns empty list when no to do items"() {
+       when:
+        def actualListItems = controller.index().todoListItems
+
+        then:
+        assert [] == actualListItems
+    }
+
+    void "index returns list with all created toDoItems"() {
+        given:
+        def todoItem = new ToDoItem(name: "something").save()
+        def todoItem2 = new ToDoItem(name: "something 2").save()
+
+        when:
+        def actualListItems = controller.index().todoListItems
+
+        then:
+        assert [todoItem, todoItem2] == actualListItems
     }
 }

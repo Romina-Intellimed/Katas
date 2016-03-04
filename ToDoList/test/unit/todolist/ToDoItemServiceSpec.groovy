06218d6156f8c3e40e6c50bf2aad82de9a938fb3
Romina-Intellimed <romina88.tb@gmail.com>
2016-03-04 15:12:09 +0200

    add unit test for saving an existing to do item

--- ToDoList/test/unit/todolist/ToDoItemServiceSpec.groovy
+++ ToDoList/test/unit/todolist/ToDoItemServiceSpec.groovy
@@ -1,5 +1,6 @@
 package todolist
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,6 +8,7 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(ToDoItemService)
+@Mock([ToDoItem])
 class ToDoItemServiceSpec extends Specification {
 
     def setup() {
@@ -15,6 +17,35 @@ class ToDoItemServiceSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
+    void "save updates properties of item with given id"() {
+        given:
+        def todoItem = new ToDoItem(name: "initial name").save(failOnError: true)
+        def todoItemData = buildToDoItemData()
+        
+        when:
+        def actualToDoItem = service.save(todoItem.id, todoItemData)
+
+        then:
+        todoItemData.name == actualToDoItem.name
+        todoItemData.description == actualToDoItem.description
+        todoItemData.location == actualToDoItem.location
+        todoItemData.startDate == actualToDoItem.startDate
+        todoItemData.endDate == actualToDoItem.endDate
+        todoItemData.repeat == actualToDoItem.repeat
+        todoItemData.remindDate == actualToDoItem.remindDate
+        todoItemData.priority == actualToDoItem.priority
+    }
+    
+    private def buildToDoItemData() {
+        def toDoItemData = [:]
+        toDoItemData.name = "new name"
+        toDoItemData.description = "description"
+        toDoItemData.location = "location"
+        toDoItemData.startDate = new Date()
+        toDoItemData.endDate = new Date()
+        toDoItemData.repeat = true
+        toDoItemData.remindDate = new Date()
+        toDoItemData.priority = PriorityType.NORMAL
+        toDoItemData
     }
 }

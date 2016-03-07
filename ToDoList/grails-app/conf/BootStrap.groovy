e84151f3d48f31d4f2741d6227062dc99c1f4320
Romina-Intellimed <romina88.tb@gmail.com>
2016-03-07 17:47:29 +0200

    add create .gsp and add participants in bootstrap

--- ToDoList/grails-app/conf/BootStrap.groovy
+++ ToDoList/grails-app/conf/BootStrap.groovy
@@ -1,11 +1,15 @@
 import todolist.PriorityType
 import todolist.ToDoItem
+import todolist.ToDoParticipant
 
 class BootStrap {
 
     def init = { servletContext ->
         new ToDoItem(name: "Learn Grails",description: "Read and practice",startDate: new Date(),endDate: new Date(),remindDate: new Date(), location: "Mozaik",priority: PriorityType.HIGH,repeat: true).save(failOnError: true)
         new ToDoItem(name: "Learn Groovy",description: "Read and practice",startDate: new Date(),endDate: new Date(),remindDate: new Date(), location: "Mozaik",priority: PriorityType.HIGH,repeat: true).save(failOnError: true)
+        new ToDoParticipant(name: "Jake Bond",emailAdress: "jake.bond@msn.com",age: 45)
+        new ToDoParticipant(name: "James Sawyer",emailAdress: "james.sawyer@gmail.com",age: 22)
+        new ToDoParticipant(name: "Billy Blancs",emailAdress: "billy.blancs@yahoo.com",age: 32)
     }
     def destroy = {
     }
---
+++ ToDoList/grails-app/views/toDoParticipant/create.gsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: romina
+  Date: 07.03.2016
+  Time: 17:45
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title></title>
+</head>
+
+<body>
+
+</body>
+</html>

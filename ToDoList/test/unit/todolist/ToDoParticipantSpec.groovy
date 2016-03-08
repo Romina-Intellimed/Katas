f1665cb9af570848df4939764c651e6a16e50944
Romina-Intellimed <romina88.tb@gmail.com>
2016-03-08 12:05:32 +0200

    add domain tests for ToDoParticipant

--- ToDoList/test/unit/todolist/ToDoParticipantSpec.groovy
+++ ToDoList/test/unit/todolist/ToDoParticipantSpec.groovy
@@ -15,6 +15,36 @@ class ToDoParticipantSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
+    void "test toString"() {
+        when: "a toDo Participant has a name"
+        def toDoParticipant=new ToDoParticipant(name: "Julian Blin")
+        then: "the toString method will display name"
+
+        toDoParticipant.toString()=="Julian Blin"
     }
+
+    void "a toDoParticipant with non empty name is valid"() {
+        given:
+        def toDoParticipant=new ToDoParticipant(name: "Julian Blin")
+
+        expect:
+        toDoParticipant.validate()
+    }
+
+    void "a toDoParticipant with no name is invalid"() {
+        given:
+        def toDoParticipant=new ToDoParticipant()
+
+        expect:
+        toDoParticipant.validate() == false
+    }
+
+    void "a toDoParticipant with no email address is valid"() {
+        given:
+        def toDoParticipant=new ToDoParticipant(name: "Default")
+
+        expect:
+        toDoParticipant.validate() == true
+    }
+
 }

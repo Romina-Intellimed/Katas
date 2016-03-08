53b9f4ee4018758bd66595502654e4adad9e061d
Romina-Intellimed <romina88.tb@gmail.com>
2016-03-08 14:33:16 +0200

    add tests for ToDoParticipantController

--- ToDoList/test/unit/todolist/ToDoParticipantControllerSpec.groovy
+++ ToDoList/test/unit/todolist/ToDoParticipantControllerSpec.groovy
@@ -1,5 +1,6 @@
 package todolist
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,9 +8,11 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(ToDoParticipantController)
+@Mock([ToDoParticipant])
 class ToDoParticipantControllerSpec extends Specification {
 
     def setup() {
+        controller.toDoParticipantService = Mock(ToDoParticipantService)
     }
 
     def cleanup() {
@@ -17,4 +20,43 @@ class ToDoParticipantControllerSpec extends Specification {
 
     void "test something"() {
     }
+
+
+    void "add calls corresponding service method"() {
+
+        when:
+        controller.addNew()
+
+        then:
+        1 * controller.toDoParticipantService.addNew(*_)
+    }
+
+    void "add calls corresponding service method with the right params"() {
+        given:
+        requestParamsReceived()
+        def expectedToParticipantData = buildExpectedToDoParticipnatData()
+
+        when:
+        controller.addNew()
+
+        then:
+        1 * controller.toDoParticipantService.addNew(expectedToParticipantData)
+    }
+
+    private buildExpectedToDoParticipnatData() {
+        def expectedToDoParticipantData = [:]
+        expectedToDoParticipantData.name = params.name
+        expectedToDoParticipantData.emailAddress = params.emailAddress
+        expectedToDoParticipantData.age = params.age
+        expectedToDoParticipantData
+    }
+
+    private void requestParamsReceived() {
+        params.id = "22"
+        params.name = "name"
+        params.emailAddress = "email@domain.com"
+        params.age = 23
+
+    }
+
 }

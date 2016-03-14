80b19fe2d8034d772a0304f13424ad9d81e30761
Romina-Intellimed <romina88.tb@gmail.com>
2016-03-14 08:45:22 +0200

    test protecting from null pointer exception: if, try catch, dereferencing

--- GroovyInAction/test/SummerSpec.groovy
+++ GroovyInAction/test/SummerSpec.groovy
@@ -1,3 +1,4 @@
+import com.thoughtworks.xstream.mapper.Mapper.Null
 import spock.lang.Specification
 
 /**
@@ -21,6 +22,25 @@ class SummerSpec extends Specification {
         assert 2 == summer.sumNamed(a:1, b:1)
         assert 3 == summer.sumNamed(a:1, b:1, c:1)
         assert 1 == summer.sumNamed(c:1)
+    }
+
+    void "test protecting from null pointer exception"(){
+        given:
+        def map=[a:[b:[c:1]]]
+        expect:
+        assert map.a.b.c==1
+
+        if(map && map.a &&map.a.x){
+            assert map.a.x.c==null
+        }
+
+        try{
+            assert map.a.x.c==null
+        } catch (NullPointerException ignore){
+
+        }
+
+        assert map?.a?.x?.c==null
     }
 
 }
